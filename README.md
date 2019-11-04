d4b2257ac6f3e3b15b1f4a9eeacd3cecb5c8e925
yl <1020827270@qq.com>
2019-11-04 17:58:57 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pipe-terminal
+An utility input/output library for terminal in new thread
---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+#! python3
+
+from setuptools import setup, find_packages
+
+setup(
+        name = 'pipe-terminal',
+        version = '0.1',
+        description = 'An utility input/output library for terminal in new thread',
+        author = 'YL',
+        url = 'https://github.com/yyyyl/pipe-terminal',
+        packages = find_packages(),
+)
