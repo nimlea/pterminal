b31007d780c8e66d67a03ef0f02c39267aa0e092
yl <1020827270@qq.com>
2019-11-20 14:30:59 +0800

    update version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'pipe-terminal',
-        version = '0.5.1',
+        version = '1.0.0',
         description = 'A simple, light-weight input/output library for terminal in new thread',
         author = 'YL',
         url = 'https://github.com/yyyyl/pipe-terminal',
