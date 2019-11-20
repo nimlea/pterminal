077c7a5d7c9b68b28a415247220422e5a1258ad7
yl <1020827270@qq.com>
2019-11-20 15:06:54 +0800

    add license

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = 'pipe-terminal',
-        version = '1.0.0',
-        description = 'A simple, light-weight input/output library for terminal in new thread',
-        author = 'YL',
-        url = 'https://github.com/yyyyl/pipe-terminal',
-        packages = find_packages(),
+    name = 'pipe-terminal',
+    version = '1.0.0',
+    description = 'A simple, light-weight input/output library for terminal in new thread',
+    author = 'YL',
+    url = 'https://github.com/yyyyl/pipe-terminal',
+    packages = find_packages(),
+    classifiers=[
+		"License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+		"Programming Language :: Python :: 3",
+    ],
 )
