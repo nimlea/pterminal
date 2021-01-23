1890db742c74cd2b7ae058131bc84e4ccf870890
nimlea <1020827270@qq.com>
2021-01-23 17:34:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(cur_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'pterminal',
-    version = '1.0.0',
+    version = '1.0.1',
     author = 'NimLea',
     url = 'https://github.com/nimlea/pterminal',
     packages = find_packages(),
